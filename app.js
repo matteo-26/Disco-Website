c6ddb52fc9b954940a8f259de851fc1c9ed6cd67
Ops26 <matteo.madotto26@gmail.com>
2023-12-04 20:43:12 +0100

    adding styles

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 import express from "express";
 import bodyParser from "body-parser";
+import dotenv from 'dotenv';
 
 const app = express();
 const port = 3000;
+dotenv.config();
+
+const scriptFont = process.env.scriptFont;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    
+    res.render("index.ejs", {
+        script: scriptFont
+    });
 })
 
 app.listen(port, () => {
